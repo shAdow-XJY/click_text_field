99e3ca99fc60702c0cbdd3230232e9fc6d856c41
shAdowplusing <1290232854@qq.com>
2023-05-18 19:57:07 +0800

    temp upload error show example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:click_text_field/click_text_field.dart';
 
@@ -47,38 +48,86 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            ClickTextField(
-                controller: textEditingController,
-                regExp: RegExp(r'people a|people b|people c'),
-                clickTextStyle: TextStyle(
-                  background: Paint()
-                    ..style = PaintingStyle.stroke
-                    ..strokeWidth = 2
-                    ..color = Colors.cyanAccent,
-                ),
-                onTapText: (clickCallBack) => {
-                  debugPrint('U click the highlight text $clickCallBack'),
-                }
-            ),
-            SuggestClickTextField(
-                controller: ClickTextEditingController(),
-                regExp: RegExp(r'people a|people b|people c|embed c|building d|人物一'),
-                textStyle: const TextStyle(
-                  color: Colors.deepPurple
-                ),
-                clickTextStyle: TextStyle(
-                  background: Paint()
-                    ..style = PaintingStyle.stroke
-                    ..strokeWidth = 2
-                    ..color = Colors.cyanAccent,
-                ),
-                onTapText: (clickCallBack) => {
-                  debugPrint('U click the highlight text $clickCallBack'),
-                }
-            ),
+            // ClickTextField(
+            //     controller: textEditingController,
+            //     regExp: RegExp(r'people a|people b|people c'),
+            //     clickTextStyle: TextStyle(
+            //       background: Paint()
+            //         ..style = PaintingStyle.stroke
+            //         ..strokeWidth = 2
+            //         ..color = Colors.cyanAccent,
+            //     ),
+            //     onTapText: (clickCallBack) => {
+            //       debugPrint('U click the highlight text $clickCallBack'),
+            //     }
+            // ),
+            // SuggestClickTextField(
+            //     controller: ClickTextEditingController(),
+            //     regExp: RegExp(r'people a|people b|people c|embed c|building d|人物一'),
+            //     textStyle: const TextStyle(
+            //       color: Colors.deepPurple
+            //     ),
+            //     clickTextStyle: TextStyle(
+            //       background: Paint()
+            //         ..style = PaintingStyle.stroke
+            //         ..strokeWidth = 2
+            //         ..color = Colors.cyanAccent,
+            //     ),
+            //     onTapText: (clickCallBack) => {
+            //       debugPrint('U click the highlight text $clickCallBack'),
+            //     }
+            // ),
+            TextField(
+              maxLines: null,
+              controller: ClickController(),
+            )
           ],
         ),
       ),
+    );
+  }
+}
+
+class ClickController extends TextEditingController {
+
+  @override
+  TextSpan buildTextSpan({required BuildContext context, TextStyle? style, required bool withComposing}) {
+
+    var spans = <InlineSpan>[];
+
+    int startIndex = 0;
+
+    for (var i = 0; i < text.length; ++i) {
+      if (text[i] == 'A') {
+        spans.add(
+          TextSpan(
+            text: text.substring(startIndex, i),
+            mouseCursor: SystemMouseCursors.click,
+            recognizer: TapGestureRecognizer()..onTap = () => debugPrint('click A'),
+          ),
+        );
+        spans.add(
+          TextSpan(
+            text: 'A',
+            mouseCursor: SystemMouseCursors.click,
+            recognizer: TapGestureRecognizer()..onTap = () => debugPrint('click A'),
+          ),
+        );
+        startIndex = i + 1;
+      }
+    }
+    if (startIndex < text.length) {
+      spans.add(
+        TextSpan(
+          text: text.substring(startIndex, text.length),
+          mouseCursor: SystemMouseCursors.text,
+        ),
+      );
+    }
+
+    return TextSpan(
+      children: spans,
+      mouseCursor: SystemMouseCursors.text,
     );
   }
 }
